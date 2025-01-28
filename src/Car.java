07c8f6814f4cb26d9fc68f3f6e0876024f29a69f
tamerlan31321414 <tom210979@gmail.com>
2025-01-28 08:04:18 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/CarGame.java

--- src/Car.java
+++ src/Car.java
@@ -11,21 +11,19 @@ public class Car {
     }
 
     public void draw(Graphics2D g2d) {
-        // Тело машины
         g2d.setColor(color);
         g2d.fillRoundRect(x, y, 50, 100, 15, 15);
 
-        // Тень
+
         g2d.setColor(new Color(0, 0, 0, 50));
         g2d.fillRoundRect(x + 5, y + 10, 40, 80, 15, 15);
 
-        // Лобовое стекло
+
         g2d.setColor(Color.CYAN);
         g2d.fillRoundRect(x + 10, y + 10, 30, 20, 10, 10);
         g2d.setColor(new Color(255, 255, 255, 50));
         g2d.fillRoundRect(x + 15, y + 12, 15, 10, 8, 8);
 
-        // Колеса
         g2d.setColor(Color.BLACK);
         g2d.fillOval(x - 5, y + 15, 15, 15);
         g2d.fillOval(x - 5, y + 70, 15, 15);
@@ -47,12 +45,12 @@ public class Car {
         g2d.setColor(Color.DARK_GRAY);
         g2d.fillRoundRect(x + 20, y + 50, 10, 5, 5, 5);
 
-        // Зеркала
+
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.fillRect(x - 10, y + 30, 5, 15);
         g2d.fillRect(x + 55, y + 30, 5, 15);
 
-        // Маленькие декоративные элементы
+
         g2d.setColor(new Color(255, 255, 255, 150));
         g2d.fillOval(x + 7, y - 8, 5, 5);
         g2d.fillOval(x + 37, y - 8, 5, 5);
